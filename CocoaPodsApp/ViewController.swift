39dbdc8528e40b86aea73e0e0e67eb76e555a60e
auhustsinovich <avgustinovich4@gmail.com>
2022-04-05 16:25:21 +0200

    StoryBoard completed

--- CocoaPodsApp/ViewController.swift
+++ CocoaPodsApp/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by Daniil on 5.04.22.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var animationView: SpringView!
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func animationButtonPressed(_ sender: Any) {
+        presetLabel.text = "preset: presetName"
+        curveLabel.text = "curve: curveName"
+        forceLabel.text = "force: \(randomiseLabel())"
+        durationLabel.text = "duration: \(randomiseLabel())"
+        delayLabel.text = "delay: \(randomiseLabel())"
+    }
+    
+    private func randomiseLabel() -> Double {
+        let doubledRandom = Double.random(in: 0.0...1.0)
+        return round(doubledRandom * 100) / 100.0
+    }
 }
 
